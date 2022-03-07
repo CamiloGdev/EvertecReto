34d5b76544170b5886694f5dd54258aa91b11e50
Camilo Galeano <chcamilog@gmail.com>
2022-03-07 01:42:20 -0500

    feat (CRUD of products): admin product view with images.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -17,27 +17,24 @@
 							</div>
 						@endif
 						<div class="row">
-
 							{{-- Card Product --}}
 							<div class="col-md-6">
 								<div class="card card-user">
 									<div class="card-body">
-										<p class="card-text">
-											<div class="author">
-												<a href="#">
-													<img src="{{ asset('img/default-avatar.png') }}" alt="image" class="avatar">
-													<h5 class="title mt-3">{{ $product->name }}</h5>
-												</a>
-												<p class="description">
-													{{ $product->ref }} <br>
-													{{ $product->price }} <br>
-													{{ $product->quantity }}
-												</p>
-											</div>
-										</p>
-                    <div class="card-description">
-                      {{ $product->description }}
-                    </div>
+                                        <div class="author">
+                                            <a href="#">
+                                                <img src="{{ asset('img/default-avatar.png') }}" alt="image" class="avatar">
+                                                <h5 class="title mt-3">{{ $product->name }}</h5>
+                                            </a>
+                                            <p class="description">
+                                                {{ $product->ref }} <br>
+                                                {{ $product->price }} <br>
+                                                {{ $product->quantity }}
+                                            </p>
+                                        </div>
+                                        <div class="card-description">
+                                          {{ $product->description }}
+                                        </div>
 									</div>
 									<div class="card-footer">
 										<div class="button-container">
@@ -50,38 +47,39 @@
 							</div>
 							{{-- End Card product --}}
 
-              {{-- Card Product --}}
-              <div class="col-md-6">
-                <div class="card card-user">
-                  <div class="card-body">
-                    <p class="card-text">
-                    <div class="author">
-                      <a href="#">
-                        <img src="{{ asset('img/default-avatar.png') }}" alt="image" class="avatar">
-                        <h5 class="title mt-3">{{ $product->name }}</h5>
-                      </a>
-                      <p class="description">
-                        {{ $product->ref }} <br>
-                        {{ $product->price }} <br>
-                        {{ $product->quantity }}
-                      </p>
-                    </div>
-                    </p>
-                    <div class="card-description">
-                      {{ $product->description }}
-                    </div>
-                  </div>
-                  <div class="card-footer">
-                    <div class="button-container">
-                      <a href="{{ route('products.index') }}" class="btn btn-sm mr-3">Back</a>
-                      <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">Edit</a>
-                      {{-- <button class="btn btn-sm btn-primary">Edit</button> --}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {{-- End Card product --}}
-
+                            {{-- Card Images --}}
+							<div class="col-md-6">
+								<div class="card card-user">
+									<div class="card-header">
+										<h4 class="card-title">Product images</h4>
+									</div>
+<!--									Crossfade
+									<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+										<div class="carousel-inner">
+											@foreach($product->images as $image)
+											<div class="carousel-item active">
+												<img src="{{ $image->url() }}" class="d-block w-100" alt="...">
+											</div>
+											@endforeach
+										</div>
+										<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+											<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+											<span class="visually-hidden">Previous</span>
+										</button>
+										<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+											<span class="carousel-control-next-icon" aria-hidden="true"></span>
+											<span class="visually-hidden">Next</span>
+										</button>
+									</div>
+									End Crossfade-->
+									<div class="card-body">
+										@foreach($product->images as $image)
+											<img class="img-fluid" style="width: 150px;" src="{{ $image->url() }}">
+										@endforeach
+									</div>
+								</div>
+							</div>
+                            {{-- End Card Images --}}
 						</div>
 					</div>
 				</div>
